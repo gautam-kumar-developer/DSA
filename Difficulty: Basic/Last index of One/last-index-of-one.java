425077483e60fce55a7ff789f1b7fa6a0e3a578e
Gautam Kumar <gautamkumar5046@outlook.com>
2026-03-22 22:23:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Last index of One/last-index-of-one.java
@@ -0,0 +1,13 @@
+// User function Template for Java
+
+class Solution {
+    public int lastIndex(String s) {
+        int ans = -1; 
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '1')  
+                ans = i;
+        }
+        
+        return ans;
+    }
+}
