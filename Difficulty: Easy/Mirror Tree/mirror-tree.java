fc98fdd738c0a02ab64fa4dbaa6c166b8217a231
Gautam Kumar <gautamkumar5046@outlook.com>
2026-06-29 11:55:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Mirror Tree/mirror-tree.java
+++ Difficulty: Easy/Mirror Tree/mirror-tree.java
@@ -1,25 +1,8 @@
-/*
-class Node
-{
-    int data;
-    Node left, right;
-    Node(int item)
-    {
-        data = item;
-        left = right = null;
-    }
-}
-*/
-
 class Solution {
     void mirror(Node root) {
-        if(root == null) {
-            return;
-        }
-        
+        if(root == null) return;
         mirror(root.left);
         mirror(root.right);
-        // Swap perform 
         Node temp = root.left;
         root.left = root.right;
         root.right = temp;
