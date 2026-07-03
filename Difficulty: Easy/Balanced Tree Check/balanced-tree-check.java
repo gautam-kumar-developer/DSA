1c9b7ba46fb40aafc1f4fd2976de167f8eec47d5
Gautam Kumar <gautamkumar5046@outlook.com>
2026-07-03 10:58:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Balanced Tree Check/balanced-tree-check.java
+++ Difficulty: Easy/Balanced Tree Check/balanced-tree-check.java
@@ -1,41 +1,19 @@
-/*
-class Node {
-    int data;
-    Node left, right;
-
-    Node(int d)
-    {
-        data = d;
-        left = right = null;
-    }
-}
-*/
-
 class Solution {
     public boolean isBalanced(Node root) {
-        return (height(root) != -1);
+         return helper(root) != -1;
     }
     
-    
-    // Here we are returning height if the tree is balanced, else -1;
-    private int height(Node root) {
-        if(root == null)
-            return 0;
-            
-        int leftHeight = height(root.left);
-        if(leftHeight == -1) {  // Immediately return -1 whenever we get the leftHeight -1
-            return -1;
-        }
+    private int helper(Node root) {
+        if(root == null) return 0;
         
-        int rightHeight = height(root.right);
-        if(rightHeight == -1) { // Immediately return -1 whenever we get the rightHeight -1
-            return -1;
-        }
+        int left = helper(root.left);
+        if (left == -1) return -1;
         
-        if(Math.abs(leftHeight - rightHeight) > 1)
-            return -1;
-            
-        return Math.max(leftHeight, rightHeight) + 1;
-            
+        int right = helper(root.right);
+        if(right == -1) return -1;
+        
+        if(Math.abs(left - right) > 1) return -1;
+        
+        return 1 + Math.max(left, right);
     }
 }
